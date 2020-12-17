96b5471561f769e988002cee23b3bcdda757e7eb
Eze Olea Figueroa <ezeoleaf@gmail.com>
2020-12-16 22:57:48 -0300

    Added day 15 part 2

---
+++ 2020/day-15/part-2/README.md
@@ -0,0 +1,21 @@
+# Day 15: Part 2: Rambunctious Recitation
+
+Impressed, the Elves issue you a challenge: determine the 30000000th number spoken. For example, given the same starting numbers as above:
+
+Given 0,3,6, the 30000000th number spoken is 175594.
+Given 1,3,2, the 30000000th number spoken is 2578.
+Given 2,1,3, the 30000000th number spoken is 3544142.
+Given 1,2,3, the 30000000th number spoken is 261214.
+Given 2,3,1, the 30000000th number spoken is 6895259.
+Given 3,2,1, the 30000000th number spoken is 18.
+Given 3,1,2, the 30000000th number spoken is 362.
+Given your starting numbers, what will be the 30000000th number spoken?
+
+Your puzzle answer was 37312.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
+
+At this point, you should return to your Advent calendar and try another puzzle.
+
+Your puzzle input was 16,11,15,0,1,7.
+
---
+++ 2020/day-15/part-2/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type game map[int][]int
+type turns []int
+
+func main() {
+	start := time.Now()
+
+	fmt.Printf("Result is %v \n", run())
+
+	log.Printf("Code took %s", time.Since(start))
+}
+
+func run() int {
+
+	f, err := os.Open("input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	g := game{}
+
+	scanner := bufio.NewScanner(f)
+
+	scanner.Scan()
+
+	return g.play(scanner.Text())
+}
+
+func (g game) play(conf string) int {
+	turn := 0
+	prevNumber := -1
+	var spokenNumber int
+	sConf := strings.Split(conf, ",")
+
+	for i := 0; i <= len(sConf); i++ {
+		turn++
+
+		if i > len(sConf)-1 {
+			spokenNumber = 0
+		} else {
+			spokenNumber, _ = strconv.Atoi(sConf[i])
+		}
+
+		g[spokenNumber] = append(g[spokenNumber], turn)
+
+		prevNumber = spokenNumber
+	}
+
+	for {
+		turn++
+		if _, ok := g[prevNumber]; ok && len(g[prevNumber]) > 1 {
+			turns := g[prevNumber]
+			spokenNumber = turns[len(turns)-1] - turns[len(turns)-2]
+		} else {
+			spokenNumber = 0
+		}
+
+		g[spokenNumber] = append(g[spokenNumber], turn)
+
+		prevNumber = spokenNumber
+
+		if turn == 30000000 {
+			return spokenNumber
+		}
+	}
+}
